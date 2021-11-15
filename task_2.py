92db7470ed20d313248ec35b6555c0fdbc8605d6
Affsyan <Affsyan@gmail.com>
2021-11-15 15:22:47 +0300

    les_1

--- task_2.py
+++ task_2.py
@@ -10,8 +10,21 @@ def print_directory_contents(sPath):
 нельзя. Данная задача показывает ваше умение работать с
 вложенными структурами.
 """
+import os
+from os.path import isfile
 
-def print_directory_contents(sPath):
-    pass
+
+def print_directory_contents(s_path):
+    list_dir = [s_path]
+    for path_dir in list_dir:
+        for name in os.listdir(path_dir):
+            if isfile(os.path.join(path_dir, name)):
+                print(f'{path_dir} {name}')
+            else:
+                list_dir.append(os.path.join(path_dir, name))
+
+
+print_directory_contents(input('Введите путь каталога: '))
+
 
 
