1edfb5be315b7afbd7e776bf9d1249bbe62b3826
Affsyan <Affsyan@gmail.com>
2021-11-19 12:36:59 +0300

    les_3

--- task_4_5.py
+++ task_4_5.py
@@ -14,11 +14,44 @@ import os
 
 
 def create_file(file_name):
+    file_name += '.txt'
     if file_name in os.listdir(os.getcwd()):
         print("Файл уже существует.")
     else:
-        with open((os.getcwd() + file_name), 'r', encoding='UTF-8') as f:
-            print(f.readlines())
+        number_list = [str(i) for i in range(1, 15)]
+        text_list = ['test' for _ in range(1, 15)]
+        print(number_list)
+        print(text_list)
+        path = os.getcwd() + '\\' + file_name
+        with open(path, 'w', encoding='UTF-8') as f:
+            f.write('\n'.join(y + x for x, y in zip(number_list, text_list)))
+        reed_file(path)
 
 
-create_file('test.txt')
+def reed_file(file_path):
+    wanted_symbol = 'test13'
+    counter = 0
+    wanted_symbol_list = []
+    with open(file_path, 'r', encoding='UTF-8') as q:
+        for line in q:
+            line = line.strip()
+            counter += 1
+            if line == wanted_symbol:
+                wanted_symbol_list.append([wanted_symbol_list, counter])
+        counter = 0
+        for _ in wanted_symbol_list:
+            if counter == 0:
+                print(f'Первое вхождение элемента {wanted_symbol} на строке {_[1]}')
+                counter += 0
+            else:
+                print(f'Элемент {wanted_symbol} найден на строке {_[1]}')
+
+    with open(file_path, 'r', encoding='UTF-8') as e:
+        old_data = e.read()
+
+    with open(file_path, 'w', encoding='UTF-8') as f:
+        new_data = old_data.replace(wanted_symbol, '123test')
+        f.write(new_data)
+
+
+create_file(input('Введите имя файла: '))
